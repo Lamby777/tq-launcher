f1522846876bb7fe8e137ec74a9d5498fad9901c
Cherry <cherry@sparklet.org>
2024-05-25 20:16:59 -0400

    egui hello world

--- src/bin/tql_gui.rs
+++ src/bin/tql_gui.rs
@@ -1,10 +1,54 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+#![allow(rustdoc::missing_crate_level_docs)] // it's an example
+
+use anyhow::Result;
+use eframe::egui;
 use tq_launcher::TqlOptions;
 
-fn main() {
-    let opts = TqlOptions { silent: false };
+fn main() -> Result<()> {
+    let _opts = TqlOptions { silent: false };
 
-    if let Err(e) = tq_launcher::run_game(opts) {
-        eprintln!("Error: {}", e);
-        std::process::exit(1);
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "TerraQuest Launcher",
+        options,
+        Box::new(|cc| {
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+            Box::<App>::default()
+        }),
+    )
+    .unwrap();
+
+    Ok(())
+}
+
+struct App {
+    version: String,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            version: "v0.0.1".to_owned(),
+        }
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("TQ Launcher GUI");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Version: ");
+                ui.text_edit_singleline(&mut self.version)
+                    .labelled_by(name_label.id);
+            });
+            ui.label(format!("Install version {}?", self.version));
+        });
     }
 }
