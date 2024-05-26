9f40f1374358cfb1f35d6894db918d555f3e258e
Cherry <cherry@sparklet.org>
2024-05-25 22:21:26 -0400

    cleanup + crate doc

--- tql_internal/src/lib.rs
+++ tql_internal/src/lib.rs
@@ -1,6 +1,18 @@
+//!
+//! Launcher for TerraQuest. Pretty much runs git commands
+//! or moves files all under the hood, but with a fancy
+//! user interface for players.
+//!
+//! Don't expect much yet, it's still in development.
+//!
+//! \- &Cherry, 2024
+//!
+#![allow(unused)]
+
 use anyhow::Result;
-pub use octocrab::models::repos::Release;
 use std::sync::OnceLock;
+
+pub use octocrab::models::repos::Release;
 
 mod paths;
 mod reqs;
@@ -12,26 +24,16 @@ const LAUNCHER_FOLDER_NAME: &str = "tq-launcher";
 const TQ_REPO_OWNER: &str = "MaxineHelsel";
 const TQ_REPO_NAME: &str = "TerraQuest";
 
-pub static SILENT: OnceLock<bool> = OnceLock::new();
-
-macro_rules! log {
-    ($($arg:tt)*) => {
-        if !SILENT.get().unwrap_or(&false) {
-            eprintln!($($arg)*);
-        }
-    };
-}
-
-pub fn run_game(opts: TqlOptions) -> Result<()> {
-    SILENT.set(opts.silent).unwrap();
-
-    let path = paths::launcher_folder();
-    dbg!(&path);
-    log!("Loading...");
-
-    Ok(())
-}
-
-pub struct TqlOptions {
-    pub silent: bool,
-}
+// pub static SILENT: OnceLock<bool> = OnceLock::new();
+//
+// macro_rules! log {
+//     ($($arg:tt)*) => {
+//         if !SILENT.get().unwrap_or(&false) {
+//             eprintln!($($arg)*);
+//         }
+//     };
+// }
+//
+// pub struct TqlOptions {
+//     pub silent: bool,
+// }
